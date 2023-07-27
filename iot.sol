6df2a15ee6f4ad98ac6b8cce4915a55550ea0a50
uvannn <136737550+uvannn@users.noreply.github.com>
2023-07-27 11:50:39 -0400

    Update iot.sol

--- iot.sol
+++ iot.sol
@@ -1,1 +1,36 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+contract iot {
+
+    mapping(address => uint) public balances;
+    uint public totalSupply;
+    uint public totalClaimed;
+    mapping(address => bool) public claimed;
+
+    constructor() {
+        totalSupply = 2000000000;
+    }
+
+    function claim() public {
+
+        require(totalClaimed < totalSupply, "AllTokensClaimed");
+        require(!claimed[msg.sender], "TokensClaimed");
+        claimed[msg.sender] = true;
+        balances[msg.sender] += 900;
+        totalClaimed += 900;
+    }
+
+    function safeTransfer(address _to, uint _amount) public {
+
+        require(_to != address(0), "UnsafeTransfer: zero address");
+        require(balances[_to] > 0, "UnsafeTransfer: zero balance");
+        require(balances[msg.sender] >= _amount, "Insufficient balance");
+        balances[msg.sender] -= _amount;
+        balances[_to] += _amount;
+    }
+    error UnsafeTransfer(address _address);
+}
+
 
